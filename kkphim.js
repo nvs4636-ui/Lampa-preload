8ff545fb614df8d2451031dfb1c9248a2249cc0d
nvs4636-ui <nvs4636@gmail.com>
2026-02-21 22:55:02 +0700

    Update kkphim.js

--- kkphim.js
+++ kkphim.js
@@ -2,14 +2,12 @@
     'use strict';
 
     function startPlugin() {
-        // 1. Kiểm tra nếu đã có menu KKPhim thì xóa đi trước khi thêm mới (để tránh đơ)
-        $('.menu__item[data-component="kkphim"]').remove();
+        // Xóa sạch các dấu vết cũ của kkphim nếu có
+        $('[data-component="kkphim"]').remove();
 
-        // 2. Đăng ký Component hiển thị phim
+        // Đăng ký Component
         Lampa.Component.add('kkphim', function(object) {
             var network = new Lampa.Reguest();
-            var scroll = new Lampa.Scroll({mask: true, over: true});
-            var items = [];
             var html = $('<div></div>');
             var body = $('<div class="category-full"></div>');
 
@@ -20,45 +18,49 @@
                 network.silent(url, function(data) {
                     var list = data.items || (data.data ? data.data.items : []);
                     html.empty().append(body);
-                    if (list.length) {
-                        list.forEach(function(item) {
-                            var card = Lampa.Template.get('card', {
-                                title: item.name,
-                                img: (item.thumb_url.indexOf('http') > -1 ? '' : 'https://phimimg.com/') + item.thumb_url
-                            });
-                            card.on('click:select', function() {
-                                // Gọi hàm phát phim
-                                Lampa.Noty.show('Đang tải link...');
-                                fetch('https://phimapi.com/phim/' + item.slug).then(r => r.json()).then(d => {
-                                    var episodes = d.episodes[0].server_data;
-                                    Lampa.Player.play({ url: episodes[0].link_m3u8, title: d.movie.name });
-                                    Lampa.Player.playlist(episodes.map(e => ({title: e.name, url: e.link_m3u8})));
-                                });
-                            });
-                            body.append(card);
+                    list.forEach(function(item) {
+                        var card = Lampa.Template.get('card', {
+                            title: item.name,
+                            img: (item.thumb_url.indexOf('http') > -1 ? '' : 'https://phimimg.com/') + item.thumb_url
                         });
-                    } else {
-                        body.append('<div class="selector" style="padding: 20px; text-align: center;">Không có dữ liệu</div>');
-                    }
+                        card.on('click:select', function() {
+                            Lampa.Noty.show('Đang lấy link...');
+                            fetch('https://phimapi.com/phim/' + item.slug).then(r => r.json()).then(d => {
+                                var episodes = d.episodes[0].server_data;
+                                Lampa.Player.play({ url: episodes[0].link_m3u8, title: d.movie.name });
+                                Lampa.Player.playlist(episodes.map(e => ({title: e.name, url: e.link_m3u8})));
+                            });
+                        });
+                        body.append(card);
+                    });
                 });
                 return html;
             };
             this.render = function() { return html; };
-            this.destroy = function() { network.clear(); scroll.destroy(); html.remove(); };
+            this.destroy = function() { network.clear(); html.remove(); };
         });
 
-        // 3. Thêm vào menu với ID riêng để tránh lỗi hiển thị như trong ảnh
-        var menu_item = {
-            title: 'KKPhim',
-            component: 'kkphim',
-            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM8 15c0-1.66 1.34-3 3-3s3 1.34 3 3-1.34 3-3 3-3-1.34-3-3z" fill="white"/></svg>',
-            id: 'kkphim'
+        // TẠO MENU VỚI CƠ CHẾ DÁN ĐÈ (FORCE)
+        var addMenu = function() {
+            var menu_item = {
+                title: 'KKPhim',
+                component: 'kkphim',
+                icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3h-2zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z" fill="white"/></svg>'
+            };
+
+            // Thêm vào danh sách menu của Lampa
+            Lampa.Menu.add(menu_item);
+            
+            // Nếu vẫn không thấy, ép nó xuất hiện sau 1 giây
+            setTimeout(function(){
+                if($('.menu__item[data-component="kkphim"]').length == 0){
+                    $('.menu__list').prepend('<li class="menu__item selector" data-component="kkphim"><div class="menu__ico">' + menu_item.icon + '</div><div class="menu__text">' + menu_item.title + '</div></li>');
+                }
+            }, 1000);
         };
 
-        // Đưa menu lên đầu hoặc vị trí dễ thấy
-        Lampa.Menu.add(menu_item);
-        
-        Lampa.Noty.show('KKPhim đã được cập nhật menu!');
+        addMenu();
+        Lampa.Noty.show('Đã sửa lỗi Menu KKPhim');
     }
 
     if (window.app_ready) startPlugin();
