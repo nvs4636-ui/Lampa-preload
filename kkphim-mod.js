31ee2d07b662c105664d3f7ee2191fcb7def7967
nvs4636-ui <nvs4636@gmail.com>
2026-02-22 00:23:50 +0700

    Update kkphim-mod.js

--- kkphim-mod.js
+++ kkphim-mod.js
@@ -1,43 +1,41 @@
 (function () {
     'use strict';
 
-    // 1. GỌI FILE GỐC CỦA HỌ VÀO TRƯỚC (IMPORT)
-    // Cách này giúp giữ nguyên toàn bộ nguồn: HDrezka, Filmix, v.v.
-    $.getScript('https://nb557.github.io/plugins/online_mod.js', function() {
-        console.log('Online Mod gốc đã nạp xong!');
-    });
-
-    // 2. CHÈN THÊM NGUỒN KKPHIM CỦA MÌNH VÀO
-    function startKKPhim() {
-        var KKPhimSource = {
-            name: 'KKPhim (VN)',
-            search: function (object, callback) {
-                var title = object.movie.original_title || object.movie.title;
-                var url = 'https://phimapi.com/v1/api/tim-kiem?keyword=' + encodeURIComponent(title);
-                
-                $.getJSON(url, function (res) {
+    // 1. ĐỊNH NGHĨA KKPHIM TRƯỚC
+    var KKPhimSource = {
+        name: 'KKPhim (VN)',
+        search: function (object, callback) {
+            var title = object.movie.original_title || object.movie.title;
+            var url = 'https://phimapi.com/v1/api/tim-kiem?keyword=' + encodeURIComponent(title);
+            
+            $.ajax({
+                url: url,
+                method: 'GET',
+                timeout: 5000,
+                success: function (res) {
                     var list = res.data ? res.data.items : [];
-                    var movie = list[0];
-                    if (movie) {
-                        $.getJSON('https://phimapi.com/phim/' + movie.slug, function (d) {
-                            var eps = d.episodes[0].server_data.map(function(e) {
-                                return {
-                                    title: 'Tập ' + e.name,
-                                    url: e.link_m3u8,
-                                    quality: 'FullHD'
-                                };
-                            });
+                    if (list[0]) {
+                        $.getJSON('https://phimapi.com/phim/' + list[0].slug, function (d) {
+                            var eps = d.episodes[0].server_data.map(e => ({
+                                title: 'Tập ' + e.name,
+                                url: e.link_m3u8,
+                                quality: 'FullHD'
+                            }));
                             callback(eps);
                         });
                     } else callback([]);
-                });
-            }
-        };
+                },
+                error: function () { callback([]); }
+            });
+        }
+    };
 
-        // Đăng ký vào hệ thống Source của Lampa
+    // 2. TẠO HÀM ĐĂNG KÝ NGUỒN
+    function initKKPhim() {
         Lampa.Listener.follow('player:search', function (e) {
             KKPhimSource.search(e, function (res) {
                 if (res.length > 0) {
+                    // Dùng unshift thay vì push để đưa lên đầu danh sách
                     e.callback([{
                         name: KKPhimSource.name,
                         method: function (call) { call(res); }
@@ -45,11 +43,19 @@
                 }
             });
         });
-        
-        console.log('KKPhim đã được "tiêm" vào danh sách nguồn!');
+        console.log('KKPhim đã sẵn sàng chen hàng!');
     }
 
-    // Đợi app sẵn sàng rồi mới chạy
-    if (window.app_ready) startKKPhim();
-    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') startKKPhim(); });
+    // 3. THỰC THI: NẠP KKPHIM TRƯỚC, NẠP HỌ SAU
+    if (window.app_ready) {
+        initKKPhim();
+        $.getScript('https://nb557.github.io/plugins/online_mod.js');
+    } else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type == 'ready') {
+                initKKPhim();
+                $.getScript('https://nb557.github.io/plugins/online_mod.js');
+            }
+        });
+    }
 })();
