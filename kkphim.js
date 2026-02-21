fb423c083a3bd99f9e2af96ad2f313c5888baa54
nvs4636-ui <nvs4636@gmail.com>
2026-02-22 00:00:49 +0700

    Update kkphim.js

--- kkphim.js
+++ kkphim.js
@@ -2,49 +2,62 @@
     'use strict';
 
     function startPlugin() {
-        // 1. Đăng ký KKPhim vào danh sách bộ lọc nguồn (Parser)
-        Lampa.Component.add('kkphim_mod', function (object) {
+        // 1. Đăng ký vào hệ thống Interaction (Để hiện trong mục Online)
+        Lampa.Interaction.add({
+            label: 'KKPhim',
+            id: 'kkphim_provider',
+            onSelect: function (object) {
+                // Khi bấm vào KKPhim trong danh sách Online
+                Lampa.Activity.push({
+                    title: 'KKPhim',
+                    component: 'kkphim_view',
+                    movie: object.movie,
+                    page: 1
+                });
+            }
+        });
+
+        // 2. Component hiển thị danh sách tập phim
+        Lampa.Component.add('kkphim_view', function (object) {
             var network = new Lampa.Reguest();
             var scroll = new Lampa.Scroll({mask: true, over: true});
-            var items = [];
             var html = $('<div></div>');
             var body = $('<div class="category-full"></div>');
 
             this.create = function () {
                 var _this = this;
-                var title = object.movie.title || object.movie.name;
+                var title = object.movie.original_title || object.movie.title;
                 var url = 'https://phimapi.com/v1/api/tim-kiem?keyword=' + encodeURIComponent(title);
+
+                Lampa.Noty.show('Đang quét KKPhim...');
 
                 network.silent(url, function (res) {
                     var list = res.data ? res.data.items : [];
-                    if (list.length > 0) {
-                        _this.showEpisodes(list[0].slug);
+                    var movie = list[0]; // Lấy kết quả sát nhất
+
+                    if (movie) {
+                        $.getJSON('https://phimapi.com/phim/' + movie.slug, function (data) {
+                            body.empty();
+                            var server = data.episodes[0].server_data;
+                            
+                            server.forEach(function (ep) {
+                                var btn = Lampa.Template.get('button_category', {title: 'Tập ' + ep.name});
+                                btn.on('click:select', function () {
+                                    Lampa.Player.play({
+                                        url: ep.link_m3u8,
+                                        title: data.movie.name + ' - ' + ep.name
+                                    });
+                                    Lampa.Player.playlist(server.map(e => ({title: 'Tập ' + e.name, url: e.link_m3u8})));
+                                });
+                                body.append(btn);
+                            });
+                            _this.check();
+                        });
                     } else {
-                        body.append('<div class="selector" style="padding: 20px; text-align: center;">Không tìm thấy phim trên KKPhim</div>');
+                        body.append('<div class="selector" style="padding: 20px; text-align: center;">Không tìm thấy nguồn</div>');
                     }
                 });
                 return this.render();
-            };
-
-            this.showEpisodes = function (slug) {
-                var _this = this;
-                $.getJSON('https://phimapi.com/phim/' + slug, function (data) {
-                    body.empty();
-                    var server = data.episodes[0].server_data;
-                    server.forEach(function (ep) {
-                        var item = Lampa.Template.get('button_category', {title: 'Tập ' + ep.name});
-                        item.on('click:select', function () {
-                            Lampa.Player.play({
-                                url: ep.link_m3u8,
-                                title: data.movie.name + ' - Tập ' + ep.name
-                            });
-                            Lampa.Player.playlist(server.map(e => ({title: 'Tập ' + e.name, url: e.link_m3u8})));
-                        });
-                        body.append(item);
-                    });
-                    // Refresh lại selector để điều khiển TV hoạt động
-                    _this.check();
-                });
             };
 
             this.check = function() {
@@ -61,25 +74,6 @@
 
             this.render = function () { return html.append(body); };
             this.destroy = function () { network.clear(); scroll.destroy(); html.remove(); };
-        });
-
-        // 2. CHÈN VÀO MỤC SOURCE (Đây là phần giúp hiện menu như Online Mod)
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type == 'complite') {
-                var btn = $('<div class="full-start__button selector"><span>KKPhim Online</span></div>');
-                
-                btn.on('click:select', function () {
-                    Lampa.Activity.push({
-                        title: 'KKPhim',
-                        component: 'kkphim_mod',
-                        movie: e.data,
-                        page: 1
-                    });
-                });
-
-                // Chèn vào đúng chỗ mà Shots và Trailers đang đứng
-                e.object.render().find('.full-start__buttons').append(btn);
-            }
         });
     }
 
