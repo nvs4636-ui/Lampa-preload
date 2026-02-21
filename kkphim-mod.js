b122b98be376cbffedfbefe3dfb8b98a8573c890
nvs4636-ui <nvs4636@gmail.com>
2026-02-22 01:14:30 +0700

    Update kkphim-mod.js

--- kkphim-mod.js
+++ kkphim-mod.js
@@ -1,58 +1,84 @@
 (function () {
     'use strict';
 
-    function startPlugin() {
-        // 1. Nạp các nguồn quốc tế trước
-        $.getScript('https://nb557.github.io/plugins/online_mod.js');
-        $.getScript('http://showy.online/m.js');
-
-        // 2. Đợi 1 chút cho các nút của họ hiện ra rồi mình mới "chen chân" vào
+    function init() {
+        // Lắng nghe sự kiện khi trang chi tiết phim được hiển thị hoàn tất
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
-                setTimeout(function() {
-                    var render = e.object.render();
+                var render = e.object.render();
+                var movie = e.data.movie;
+                var title = movie.title || movie.name;
+
+                // Sử dụng Interval để "canh" hàng nút xuất hiện (vì Lampa render nút hơi trễ)
+                var waitButtons = setInterval(function() {
                     var container = render.find('.full-start__buttons');
                     
-                    // Kiểm tra nếu nút KKPhim chưa tồn tại thì mới chèn (tránh trùng lặp)
-                    if (container.length && !container.find('.btn--kkphim').length) {
-                        var title = e.data.movie.title || e.data.movie.name;
-                        
-                        var btn = $('<div class="full-start__button selector btn--kkphim" style="background-color: #f39c12 !important; color: #fff !important; border-radius: 5px; margin-left: 10px;">' +
-                                        '<span>KKPhim VN</span>' +
-                                    '</div>');
+                    // Nếu thấy hàng nút và chưa có nút KKPhim thì chèn vào
+                    if (container.length) {
+                        if (!container.find('.view--kkphim').length) {
+                            
+                            // Tạo nút bấm chuẩn class Lampa để di chuyển remote mượt mà
+                            var btn = $('<div class="full-start__button selector view--kkphim" style="background-color: #e67e22 !important; color: #fff !important; border-radius: 6px; margin-left: 10px;">' +
+                                            '<span>KKPhim 4K</span>' +
+                                        '</div>');
 
-                        btn.on('click:select', function () {
-                            Lampa.Noty.show('Đang tìm: ' + title);
-                            $.getJSON('https://phimapi.com/v1/api/tim-kiem?keyword=' + encodeURIComponent(title), function (res) {
-                                if (res.data && res.data.items[0]) {
-                                    $.getJSON('https://phimapi.com/phim/' + res.data.items[0].slug, function (d) {
-                                        var eps = d.episodes[0].server_data;
-                                        Lampa.Select.show({
-                                            title: d.movie.name,
-                                            items: eps.map(i => ({ title: 'Tập ' + i.name, url: i.link_m3u8 })),
-                                            onSelect: function (sel) {
-                                                Lampa.Player.play({ url: sel.url, title: d.movie.name + ' - ' + sel.title });
-                                            }
+                            // Xử lý sự kiện bấm nút
+                            btn.on('click:select', function () {
+                                Lampa.Noty.show('Đang quét nguồn KKPhim...');
+                                
+                                var searchUrl = 'https://phimapi.com/v1/api/tim-kiem?keyword=' + encodeURIComponent(title);
+                                
+                                $.getJSON(searchUrl, function (res) {
+                                    var found = res.data ? res.data.items[0] : null;
+                                    if (found) {
+                                        $.getJSON('https://phimapi.com/phim/' + found.slug, function (detail) {
+                                            var eps = detail.episodes[0].server_data;
+                                            
+                                            // Hiển thị danh sách tập để chọn
+                                            Lampa.Select.show({
+                                                title: detail.movie.name,
+                                                items: eps.map(function(item) {
+                                                    return { title: 'Tập ' + item.name, url: item.link_m3u8 };
+                                                }),
+                                                onSelect: function (selected) {
+                                                    Lampa.Player.play({
+                                                        url: selected.url,
+                                                        title: detail.movie.name + ' - ' + selected.title
+                                                    });
+                                                    // Thêm danh sách phát để chuyển tập nhanh
+                                                    Lampa.Player.playlist(eps.map(i => ({ title: 'Tập ' + i.name, url: i.link_m3u8 })));
+                                                }
+                                            });
                                         });
-                                    });
-                                } else { Lampa.Noty.show('Không tìm thấy nguồn!'); }
+                                    } else {
+                                        Lampa.Noty.show('Không tìm thấy phim này trên KKPhim!');
+                                    }
+                                });
                             });
-                        });
 
-                        container.append(btn);
-                        // Ép Remote nhận diện lại hàng nút
-                        Lampa.Controller.add('full', {
-                            toggle: function() {
-                                Lampa.Controller.collectionSet(render);
-                                Lampa.Controller.toggle('full');
-                            }
-                        });
+                            // Chèn nút vào hàng
+                            container.append(btn);
+                            
+                            // Cập nhật lại điều khiển Remote cho hàng nút
+                            Lampa.Controller.add('full', {
+                                toggle: function() {
+                                    Lampa.Controller.collectionSet(render);
+                                    Lampa.Controller.toggle('full');
+                                }
+                            });
+                        }
+                        // Dừng vòng lặp sau khi đã chèn xong
+                        clearInterval(waitButtons);
                     }
-                }, 1000); // Đợi 1 giây cho chắc cú
+                }, 200);
+
+                // Tự hủy vòng lặp sau 5 giây để tránh tốn tài nguyên nếu không tìm thấy chỗ chèn
+                setTimeout(function() { clearInterval(waitButtons); }, 5000);
             }
         });
     }
 
-    if (window.app_ready) startPlugin();
-    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') startPlugin(); });
+    // Chạy plugin khi ứng dụng đã sẵn sàng
+    if (window.app_ready) init();
+    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') init(); });
 })();
