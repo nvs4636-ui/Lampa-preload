047214f6811d680b50524868fd36b084bd21ff55
nvs4636-ui <nvs4636@gmail.com>
2026-02-21 23:06:21 +0700

    Update kkphim.js

--- kkphim.js
+++ kkphim.js
@@ -1,66 +1,67 @@
 (function () {
     'use strict';
 
-    function startPlugin() {
-        // Xóa sạch các dấu vết cũ của kkphim nếu có
-        $('[data-component="kkphim"]').remove();
+    function KKPhimProvider() {
+        var network = new Lampa.Reguest();
 
-        // Đăng ký Component
-        Lampa.Component.add('kkphim', function(object) {
-            var network = new Lampa.Reguest();
-            var html = $('<div></div>');
-            var body = $('<div class="category-full"></div>');
-
-            this.create = function() {
-                var _this = this;
-                var url = object.search ? 'https://phimapi.com/v1/api/tim-kiem?keyword=' + encodeURIComponent(object.search) : 'https://phimapi.com/danh-sach/phim-moi-cap-nhat?page=1';
-                
-                network.silent(url, function(data) {
-                    var list = data.items || (data.data ? data.data.items : []);
-                    html.empty().append(body);
-                    list.forEach(function(item) {
-                        var card = Lampa.Template.get('card', {
-                            title: item.name,
-                            img: (item.thumb_url.indexOf('http') > -1 ? '' : 'https://phimimg.com/') + item.thumb_url
-                        });
-                        card.on('click:select', function() {
-                            Lampa.Noty.show('Đang lấy link...');
-                            fetch('https://phimapi.com/phim/' + item.slug).then(r => r.json()).then(d => {
-                                var episodes = d.episodes[0].server_data;
-                                Lampa.Player.play({ url: episodes[0].link_m3u8, title: d.movie.name });
-                                Lampa.Player.playlist(episodes.map(e => ({title: e.name, url: e.link_m3u8})));
-                            });
-                        });
-                        body.append(card);
-                    });
-                });
-                return html;
-            };
-            this.render = function() { return html; };
-            this.destroy = function() { network.clear(); html.remove(); };
-        });
-
-        // TẠO MENU VỚI CƠ CHẾ DÁN ĐÈ (FORCE)
-        var addMenu = function() {
-            var menu_item = {
-                title: 'KKPhim',
-                component: 'kkphim',
-                icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3h-2zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z" fill="white"/></svg>'
-            };
-
-            // Thêm vào danh sách menu của Lampa
-            Lampa.Menu.add(menu_item);
+        // Hàm này bóc tách dữ liệu từ KKPhim về chuẩn Lampa
+        function extractVideo(data) {
+            var movie = data.movie;
+            var episodes = data.episodes[0].server_data;
             
-            // Nếu vẫn không thấy, ép nó xuất hiện sau 1 giây
-            setTimeout(function(){
-                if($('.menu__item[data-component="kkphim"]').length == 0){
-                    $('.menu__list').prepend('<li class="menu__item selector" data-component="kkphim"><div class="menu__ico">' + menu_item.icon + '</div><div class="menu__text">' + menu_item.title + '</div></li>');
-                }
-            }, 1000);
-        };
+            return episodes.map(function(ep) {
+                return {
+                    title: ep.name,
+                    url: ep.link_m3u8,
+                    quality: 'Full HD' // KKPhim thường là chất lượng tốt
+                };
+            });
+        }
 
-        addMenu();
-        Lampa.Noty.show('Đã sửa lỗi Menu KKPhim');
+        // Đăng ký vào hệ thống tìm kiếm nguồn của Lampa
+        this.search = function (object, callback) {
+            if (object.movie.title) {
+                var searchUrl = 'https://phimapi.com/v1/api/tim-kiem?keyword=' + encodeURIComponent(object.movie.title);
+                
+                network.silent(searchUrl, function (result) {
+                    var list = result.data ? result.data.items : [];
+                    var found = list.filter(function(i) {
+                        // So khớp tên phim để tìm đúng nguồn
+                        return i.name.toLowerCase() === object.movie.title.toLowerCase() || 
+                               i.origin_name.toLowerCase() === object.movie.title.toLowerCase();
+                    });
+
+                    if (found.length > 0) {
+                        // Nếu tìm thấy phim trên KKPhim, tạo một "thẻ" nguồn
+                        callback([{
+                            name: 'KKPhim',
+                            method: function(call_success) {
+                                fetch('https://phimapi.com/phim/' + found[0].slug)
+                                    .then(res => res.json())
+                                    .then(json => {
+                                        call_success(extractVideo(json));
+                                    });
+                            }
+                        }]);
+                    } else {
+                        callback([]); // Không tìm thấy thì thôi
+                    }
+                });
+            }
+        };
+    }
+
+    function startPlugin() {
+        // Đăng ký KKPhim như một "Nguồn video" (Buttons trong thẻ phim)
+        Lampa.Internal.listen('player:search', function (object) {
+            var provider = new KKPhimProvider();
+            provider.search(object, function (sources) {
+                // Trả về danh sách server cho Lampa hiển thị
+                object.callback(sources);
+            });
+        });
+        
+        Lampa.Noty.show('KKPhim đã được tích hợp làm nguồn phim!');
     }
 
     if (window.app_ready) startPlugin();
